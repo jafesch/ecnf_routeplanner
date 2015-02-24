b1b646cf88409d2d83161bc038bdc96735c45d97
Julian Fisch <julian.fisch@asinfotrack.ch>
2015-02-24 16:37:12 +0100

    added WayPoint to RoutePlannerLib

--- RoutePlannerConsole/RoutePlannerConsoleApp.cs
+++ RoutePlannerConsole/RoutePlannerConsoleApp.cs
@@ -4,14 +4,21 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib;
 
-namespace RoutePlannerConsole
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerConsole
 {
     class RoutePlannerConsoleApp
     {
         static void Main(string[] args)
         {
+            //Aufgabe 1
             Console.WriteLine("Welcome to RoutePlanner (Version " + Assembly.GetExecutingAssembly().GetName().Version + ")");
+
+            //Aufgabe 2d
+            var wayPoint = new WayPoint("Windisch", 47.479319847061966, 8.212966918945312);
+            Console.WriteLine("{0}: {1}/{2}", wayPoint.Name, wayPoint.Latitude, wayPoint.Longitude);
+
             Console.ReadLine();
         }
     }
