4a26f77f6f94bc6155b84d7c7248c2de1002ad3a
Julian Fisch <julian.fisch@students.fhnw.ch>
2015-03-09 18:26:13 +0100

    lab 4 excersize 1 a,b,c

--- RoutePlannerLib/Cities.cs
+++ RoutePlannerLib/Cities.cs
@@ -67,5 +67,34 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 
             return citiesTemp;
         }
+
+        #region Lab04: FindShortestPath helper function
+        /// <summary>
+        /// Find all cities between 2 cities 
+        /// </summary>
+        /// <param name="from">source city</param>
+        /// <param name="to">target city</param>
+        /// <returns>list of cities</returns>
+        public List<City> FindCitiesBetween(City from, City to)
+        {
+            var foundCities = new List<City>();
+            if (from == null || to == null)
+                return foundCities;
+
+            foundCities.Add(from);
+
+            var minLat = Math.Min(from.Location.Latitude, to.Location.Latitude);
+            var maxLat = Math.Max(from.Location.Latitude, to.Location.Latitude);
+            var minLon = Math.Min(from.Location.Longitude, to.Location.Longitude);
+            var maxLon = Math.Max(from.Location.Longitude, to.Location.Longitude);
+
+            foundCities.AddRange(cities.FindAll(c =>
+                c.Location.Latitude > minLat && c.Location.Latitude < maxLat
+                        && c.Location.Longitude > minLon && c.Location.Longitude < maxLon));
+
+            foundCities.Add(to);
+            return foundCities;
+        }
+        #endregion
     }
 }
