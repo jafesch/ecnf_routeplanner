0c990085f1b22720deb57728bfb50c5866a15b3c
Timon Krebs <timon.krebs@students.fhnw.ch>
2015-03-09 22:54:01 +0100

    updated TextReaderExtension

--- RoutePlannerLib/Cities.cs
+++ RoutePlannerLib/Cities.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util;
 
 namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
 {
@@ -26,22 +27,22 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
         public int ReadCities(string filename)
         {
             int count = 0;
-
-            TextReader reader = File.OpenText(filename);
-
-            String s;
-            Char[] separator = { '\t' };
-            City city;
-            while ((s = reader.ReadLine()) != null)
+            using (TextReader reader = new StreamReader(filename))
             {
-                count++;
-                city = new City(s.Split(separator)[0],
-                    s.Split(separator)[1],
-                    int.Parse(s.Split(separator)[2]),
-                    double.Parse(s.Split(separator)[3]),
-                    double.Parse(s.Split(separator)[4])
-                    );
-                cities.Add(city);
+                IEnumerable<string[]> citiesAsStrings = reader.GetSplittedLines('\t');
+                foreach (string[] cs in citiesAsStrings)
+                {
+                    City city = new City(
+                        cs[0].Trim(),
+                        cs[1].Trim(),
+                        int.Parse(cs[2]),
+                        double.Parse(cs[3]),
+                        double.Parse(cs[4])
+                        );
+                    cities.Add(city);
+                    count++;
+                }
+
             }
             return count;
         }
