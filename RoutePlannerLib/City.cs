dda534740b23b4633bca7bb851994fa75df8875f
Julian Fisch <julian.fisch@students.fhnw.ch>
2015-03-09 18:27:38 +0100

    forgot to sace city

--- RoutePlannerLib/City.cs
+++ RoutePlannerLib/City.cs
@@ -20,5 +20,12 @@ namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
             Population = _population;
             Location = new WayPoint(_name, _latitude, _longitude);
         }
+
+        public override bool Equals(object obj)
+        {
+            var city = obj as City;
+            if (city == null) return false;
+            return this.Name == city.Name && this.Country == city.Country;
+        }
     }
 }
