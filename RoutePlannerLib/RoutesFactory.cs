a416a13138e7a60cbd8e936eb873afa459e26aca
Julian Fisch <julian.fisch@students.fhnw.ch>
2015-03-17 15:54:41 +0100

    Finished Lab 5 Aufgabe 1

---
+++ RoutePlannerLib/RoutesFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+{
+    public class RoutesFactory
+    {
+        static public IRoutes Create(Cities cities)
+        {
+            var routeAlgorithm = Properties.Settings.Default.RouteAlgorithm;
+            return Create(cities, routeAlgorithm);
+        }
+        static public IRoutes Create(Cities cities, string algorithmClassName)
+        {
+            IRoutes routeFactory = null;
+            Assembly a = Assembly.LoadFrom("RoutePlannerLib.dll");
+            Type t = a.GetType(algorithmClassName);
+
+            if (t == null)
+            {
+                return null;
+            }
+
+            routeFactory = (IRoutes)Activator.CreateInstance(t, cities);
+            return routeFactory as IRoutes;
+        }
+    }
+}
