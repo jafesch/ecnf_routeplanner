737653512d539d0bde7e8048433ac7917657fe3d
Julian Fisch <julian.fisch@students.fhnw.ch>
2015-04-26 19:28:45 +0200

    created folders for namespaces

--- RoutePlannerLib/Util/SimpleObjectWriter.cs
+++ RoutePlannerLib/Util/SimpleObjectWriter.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib
+namespace Fhnw.Ecnf.RoutePlanner.RoutePlannerLib.Util
 {
     public class SimpleObjectWriter
     {
